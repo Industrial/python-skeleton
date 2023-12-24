1a7247f1de4bf156dba101f061547192155a4eb2
Tom Wieland <tom.wieland@gmail.com>
2023-12-24 12:44:23 +0100

    feat: zellij, flake, vscode updates

--- flake.nix
+++ flake.nix
@@ -1,33 +1,34 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
-  };
-
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
   outputs = {
     self,
     nixpkgs,
     flake-utils,
-    flake-compat,
   } @ inputs: let
     inherit (nixpkgs) lib;
     inherit (lib) recursiveUpdate;
     inherit (flake-utils.lib) eachDefaultSystem defaultSystems;
-
     nixpkgsFor = lib.genAttrs defaultSystems (system:
       import nixpkgs {
         inherit system;
+        config = {
+          allowUnfree = true;
+        };
       });
   in (eachDefaultSystem (
     system: let
       pkgs = nixpkgsFor.${system};
     in {
-      devShell = pkgs.mkShell {
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          bashInteractive
+        ];
         buildInputs = with pkgs; [
+          # File Watch (inotifywatch)
+          inotify-tools
+
+          # Python
           python310
           poetry
         ];
